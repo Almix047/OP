7b6c0423fb180794976d91e53b87f48479240c65
Almix047 <Amix047@gmail.com>
2020-03-12 20:39:26 +0300

    Splitted Description

--- output_list.rb
+++ output_list.rb
@@ -3,7 +3,7 @@
 # Configuration of the output view
 class OutputList
   HEADER = %w[
-    Name Description Price Link
+    Name Diagonal Screen CPU RAM Storage Video System Color Price Link
     Offers Discont Offers_Link
     Second_Hand_Price Second_Hand_Link
     Reviews Rated Reviews_link
@@ -26,14 +26,18 @@ class OutputList
   def prepare_list_data
     products.flatten.each_with_index do |product, index|
       row = [
-        product.name, product.description, product.price, product.link,
+        product.name, split_description(product.description), product.price, product.link,
         product.offers, product.discont, product.offers_link,
         product.second_hand_price, product.second_hand_link,
         product.reviews, product.rated, product.reviews_link,
         product.discussion, product.discussion_link
-      ]
+      ].flatten
       @rows.push(row)
     end
     @rows.unshift(HEADER)
+  end
+
+  def split_description(text)
+    text.split(',')
   end
 end
